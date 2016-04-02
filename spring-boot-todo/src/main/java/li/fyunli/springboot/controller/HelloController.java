c07d7863c7f6df3b99571dbdd3977f0a2b555d0b
fyunli <fyunli@gmail.com>
2016-04-02 17:14:11 +0800

    freemarker support

--- spring-boot-todo/src/main/java/li/fyunli/springboot/controller/HelloController.java
+++ spring-boot-todo/src/main/java/li/fyunli/springboot/controller/HelloController.java
@@ -1,17 +1,26 @@
 package li.fyunli.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * Created by fyunli on 16/4/1.
  */
-@RestController
+@Controller
 public class HelloController {
 
+    @Value("${application.message:Hello World}")
+    private String message = "Hello World";
+
     @RequestMapping("/hello")
-    public String hello(){
-        return "Hello Boot!";
+    public String welcome(ModelMap model) {
+        model.put("time", new Date());
+        model.put("message", this.message);
+        return "hello";
     }
 
 }
